db83fee704e790828cfcb4cc6adc948824498b4e
VictorFerreira999 <victorferreira9990@gmail.com>
2025-01-09 07:47:20 -0300

    Conexão realizada

---
+++ estoquepreco/src/main/java/com/microservico/estoquepreco/constantes/RabbitmqConstantes.java
@@ -0,0 +1,7 @@
+package com.microservico.estoquepreco.constantes;
+
+public class RabbitmqConstantes {
+    public static final String FILA_ESTOQUE = "ESTOQUE";
+    public static final String FILA_PRECO = "PRECO";
+
+}
