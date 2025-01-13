37903d3985194c75c5f6fcb7d89c5d61649ffdbc
VictorFerreira999 <victorferreira9990@gmail.com>
2025-01-13 09:18:34 -0300

    consumer

--- consumidorestoque/src/main/java/com/microsservico/consumidorestoque/consumer/EstoqueConsumer.java
+++ consumidorestoque/src/main/java/com/microsservico/consumidorestoque/consumer/EstoqueConsumer.java
@@ -1,4 +1,17 @@
 package com.microsservico.consumidorestoque.consumer;
 
+import constantes.RabbitmqConstantes;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import dto.EstoqueDto;
+
+@Component
 public class EstoqueConsumer {
+
+    @RabbitListener(queues = RabbitmqConstantes.FILA_ESTOQUE)
+    private void consumidor(EstoqueDto estoqueDto){
+        System.out.println(estoqueDto.codigoproduto);
+        System.out.println(estoqueDto.quantidade);
+        System.out.println("_______________________________");
+    }
 }
